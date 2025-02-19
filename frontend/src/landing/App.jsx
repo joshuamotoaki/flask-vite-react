472d9d8ec1446a39947491a851ecfc317d44057c
joshuamotoaki <dachshunduniverse@gmail.com>
2025-02-18 19:21:56 -0500

    Add flags at top of screen for debug vs production

--- frontend/src/landing/App.jsx
+++ frontend/src/landing/App.jsx
@@ -1,10 +1,10 @@
 function App() {
-    return (
-      <div>
-        <h1>Landing Page</h1>
-        <p>Welcome to the public landing page!</p>
-      </div>
-    )
-  }
-  
-  export default App
+  return (
+    <div>
+      <h1>Landing Page</h1>
+      <p>Welcome to the public landing page!</p>
+    </div>
+  );
+}
+
+export default App;
--- frontend/src/protected/App.jsx
+++ frontend/src/protected/App.jsx
@@ -1,10 +1,10 @@
 function App() {
-    return (
-      <div>
-        <h1>Protected Area</h1>
-        <p>Welcome to the protected area. You must be logged in to see this.</p>
-      </div>
-    )
-  }
-  
-  export default App
+  return (
+    <div>
+      <h1>Protected Area</h1>
+      <p>Welcome to the protected area. You must be logged in to see this.</p>
+    </div>
+  );
+}
+
+export default App;
